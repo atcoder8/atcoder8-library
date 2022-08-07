ecd7b6ba071ab2cd748e11c680cf82ea79f17fac
atcoder8 <atcoder8@gmail.com>
2022-08-07 09:44:55 +0900

    Updated factorial.rs

--- src/factorial.rs
+++ src/factorial.rs
@@ -1,76 +1,74 @@
-pub mod factorial {
-    use std::ops::{Div, Mul};
+use std::ops::{Div, Mul};
 
-    pub struct Factorial<T> {
-        fac: Vec<T>,
+pub struct Factorial<T> {
+    fac: Vec<T>,
+}
+
+impl<T> Default for Factorial<T>
+where
+    T: Clone + From<usize> + Mul<Output = T> + Div<Output = T>,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> Factorial<T>
+where
+    T: Clone + From<usize> + Mul<Output = T> + Div<Output = T>,
+{
+    /// Constructs a new, empty `Factorial<T>`.
+    pub fn new() -> Self {
+        Self {
+            fac: vec![T::from(1)],
+        }
     }
 
-    impl<T> Default for Factorial<T>
-    where
-        T: Clone + From<usize> + Mul<Output = T> + Div<Output = T>,
-    {
-        fn default() -> Self {
-            Self::new()
+    /// Returns the factorial of `n`.
+    pub fn factorial(&mut self, n: usize) -> T {
+        if self.fac.len() < n + 1 {
+            for i in (self.fac.len() - 1)..n {
+                self.fac.push(self.fac[i].clone() * (i + 1).into());
+            }
+        }
+        self.fac[n].clone()
+    }
+
+    /// Returns the number of choices when selecting `k` from `n` and arranging them in a row.
+    pub fn permutations(&mut self, n: usize, k: usize) -> T {
+        if n < k {
+            T::from(0)
+        } else {
+            self.factorial(n) / self.factorial(n - k)
         }
     }
 
-    impl<T> Factorial<T>
-    where
-        T: Clone + From<usize> + Mul<Output = T> + Div<Output = T>,
-    {
-        /// Constructs a new, empty `Factorial<T>`.
-        pub fn new() -> Self {
-            Self {
-                fac: vec![T::from(1)],
-            }
+    /// Returns the number of choices to select `k` from `n`.
+    pub fn combinations(&mut self, n: usize, k: usize) -> T {
+        if n < k {
+            T::from(0)
+        } else {
+            self.permutations(n, k) / self.factorial(k)
         }
+    }
 
-        /// Returns the factorial of `n`.
-        pub fn factorial(&mut self, n: usize) -> T {
-            if self.fac.len() < n + 1 {
-                for i in (self.fac.len() - 1)..n {
-                    self.fac.push(self.fac[i].clone() * (i + 1).into());
-                }
-            }
-            self.fac[n].clone()
-        }
-
-        /// Returns the number of choices when selecting `k` from `n` and arranging them in a row.
-        pub fn permutations(&mut self, n: usize, k: usize) -> T {
-            if n < k {
+    /// Calculate the number of cases when sample of `k` elements from a set of `n` elements, allowing for duplicates.
+    pub fn combinations_with_repetition(&mut self, n: usize, k: usize) -> T {
+        if n == 0 {
+            if k == 0 {
+                T::from(1)
+            } else {
                 T::from(0)
-            } else {
-                self.factorial(n) / self.factorial(n - k)
             }
-        }
-
-        /// Returns the number of choices to select `k` from `n`.
-        pub fn combinations(&mut self, n: usize, k: usize) -> T {
-            if n < k {
-                T::from(0)
-            } else {
-                self.permutations(n, k) / self.factorial(k)
-            }
-        }
-
-        /// Calculate the number of cases when sample of `k` elements from a set of `n` elements, allowing for duplicates.
-        pub fn combinations_with_repetition(&mut self, n: usize, k: usize) -> T {
-            if n == 0 {
-                if k == 0 {
-                    T::from(1)
-                } else {
-                    T::from(0)
-                }
-            } else {
-                self.combinations(n + k - 1, k)
-            }
+        } else {
+            self.combinations(n + k - 1, k)
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::factorial::*;
+    use super::Factorial;
     use num::BigUint;
     use std::str::FromStr;
 
