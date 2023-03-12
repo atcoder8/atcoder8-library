b9812b1d32f02589f40a367934f9747407bdb445
atcoder8 <atcoder8@gmail.com>
2023-03-12 19:46:12 +0900

    Added operations involving remainders to module `matrix`.

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
+use std::ops::{Add, AddAssign, Mul, MulAssign, Rem, RemAssign, Sub, SubAssign};
 
 use num::{One, Zero};
 
@@ -9,6 +9,13 @@ where
 {
     shape: (usize, usize),
     flattened: Vec<T>,
+}
+
+pub trait Transpose<T>
+where
+    T: Clone,
+{
+    fn transposed(&self) -> Self;
 }
 
 pub trait Identity<T>
@@ -39,11 +46,44 @@ where
     fn mat_pow(&self, exp: usize) -> Self;
 }
 
-pub trait Transpose<T>
+pub trait MatMulMod<T>
+where
+    T: Clone + Add<T, Output = T> + Mul<T, Output = T> + Rem<T, Output = T>,
+{
+    fn mat_mul_mod(&self, rhs: &Self, modulus: T) -> Self;
+}
+
+pub trait MatMulAssignMod<T>
+where
+    T: Clone + Add<T, Output = T> + Mul<T, Output = T> + Rem<T, Output = T>,
+{
+    fn mat_mul_assign_mod(&mut self, rhs: &Self, modulus: T);
+}
+
+pub trait MatPowMod<T>
+where
+    T: Clone + Zero + One + Add<T, Output = T> + Mul<T, Output = T> + Rem<T, Output = T>,
+{
+    fn mat_pow_mod(&self, exp: usize, modulus: T) -> Self;
+}
+
+impl<T> Transpose<T> for Matrix<T>
 where
     T: Clone,
 {
-    fn transposed(&self) -> Self;
+    fn transposed(&self) -> Self {
+        let mut flattened = vec![];
+
+        for i in 0..self.elem_num() {
+            let coord = (i % self.shape.0, i / self.shape.0);
+            flattened.push(self.flattened[self.coord_to_idx(coord)].clone());
+        }
+
+        Self {
+            shape: (self.shape.1, self.shape.0),
+            flattened,
+        }
+    }
 }
 
 impl<T> Identity<T> for Matrix<T>
@@ -76,15 +116,10 @@ where
         let calc_elem = |coord: (usize, usize)| {
             let (i, j) = coord;
 
-            let mut elem = self.flattened[self.coord_to_idx((i, 0))].clone()
-                * rhs.flattened[rhs.coord_to_idx((0, j))].clone();
-            for k in 1..w1 {
-                elem = elem
-                    + self.flattened[self.coord_to_idx((i, k))].clone()
-                        * rhs.flattened[rhs.coord_to_idx((k, j))].clone();
-            }
-
-            elem
+            (0..w1)
+                .map(|k| self.get((i, k)).clone() * rhs.get((k, j)).clone())
+                .reduce(|acc, x| acc + x)
+                .unwrap()
         };
 
         let flattened: Vec<T> = (0..(h1 * w2))
@@ -131,147 +166,71 @@ where
     }
 }
 
-impl<T> Transpose<T> for Matrix<T>
+impl<T> MatMulMod<T> for Matrix<T>
 where
-    T: Clone,
+    T: Clone + Add<T, Output = T> + Mul<T, Output = T> + Rem<T, Output = T>,
 {
-    fn transposed(&self) -> Self {
-        let mut flattened = vec![];
+    fn mat_mul_mod(&self, rhs: &Self, modulus: T) -> Self {
+        let (h1, w1) = self.shape;
+        let (h2, w2) = rhs.shape;
 
-        for i in 0..self.elem_num() {
-            let coord = (i % self.shape.0, i / self.shape.0);
-            flattened.push(self.flattened[self.coord_to_idx(coord)].clone());
-        }
+        assert_eq!(w1, h2);
+
+        let lhs = self.clone() % modulus.clone();
+        let rhs = rhs.clone() % modulus.clone();
+
+        let calc_elem = |coord: (usize, usize)| {
+            let (i, j) = coord;
+
+            let elem = (0..w1)
+                .map(|k| lhs.get((i, k)).clone() * rhs.get((k, j)).clone() % modulus.clone())
+                .reduce(|acc, x| (acc + x) % modulus.clone())
+                .unwrap();
+
+            (elem + modulus.clone()) % modulus.clone()
+        };
+
+        let flattened: Vec<T> = (0..(h1 * w2))
+            .map(|idx| calc_elem((idx / w2, idx % w2)))
+            .collect();
 
         Self {
-            shape: (self.shape.1, self.shape.0),
+            shape: (h1, w2),
             flattened,
         }
     }
 }
 
-impl<T> Matrix<T>
+impl<T> MatMulAssignMod<T> for Matrix<T>
 where
-    T: Clone,
+    T: Clone + Add<T, Output = T> + Mul<T, Output = T> + Rem<T, Output = T>,
 {
-    pub fn new(shape: (usize, usize)) -> Self
-    where
-        T: Default,
-    {
-        assert!(shape.0 >= 1 && shape.1 >= 1);
-
-        Self {
-            shape,
-            flattened: vec![T::default(); shape.0 * shape.1],
-        }
+    fn mat_mul_assign_mod(&mut self, rhs: &Self, modulus: T) {
+        *self = self.mat_mul_mod(rhs, modulus);
     }
+}
 
-    pub fn from_flattened(shape: (usize, usize), flattened: Vec<T>) -> Self {
-        assert!(shape.0 >= 1 && shape.1 >= 1);
-        assert_eq!(shape.0 * shape.1, flattened.len());
+impl<T> MatPowMod<T> for Matrix<T>
+where
+    T: Clone + Zero + One + Add<T, Output = T> + Mul<T, Output = T> + Rem<T, Output = T>,
+{
+    fn mat_pow_mod(&self, exp: usize, modulus: T) -> Self {
+        assert!(self.is_square());
 
-        Self { shape, flattened }
-    }
+        let mut ret = Self::identity(self.shape.0);
+        let mut mul = self.clone();
+        let mut exp = exp;
 
-    pub fn filled(shape: (usize, usize), x: T) -> Self {
-        Self::from_flattened(shape, vec![x.clone(); shape.0 * shape.1])
-    }
-
-    pub fn zero(shape: (usize, usize)) -> Self
-    where
-        T: Zero,
-    {
-        Self::from_flattened(shape, vec![T::zero(); shape.0 * shape.1])
-    }
-
-    pub fn one(shape: (usize, usize)) -> Self
-    where
-        T: One,
-    {
-        Self::from_flattened(shape, vec![T::one(); shape.0 * shape.1])
-    }
-
-    pub fn from_vector(vec: Vec<T>) -> Self {
-        Self {
-            shape: (vec.len(), 1),
-            flattened: vec,
-        }
-    }
-
-    pub fn shape(&self) -> (usize, usize) {
-        self.shape
-    }
-
-    pub fn flattened(self) -> Vec<T> {
-        self.flattened
-    }
-
-    pub fn elem_num(&self) -> usize {
-        self.shape.0 * self.shape.1
-    }
-
-    pub fn to_vec(self) -> Vec<Vec<T>> {
-        let (h, w) = self.shape;
-
-        let mut mat = vec![vec![]; h];
-        mat.iter_mut().for_each(|x| x.reserve(w));
-
-        for (i, elem) in self.flattened.into_iter().enumerate() {
-            mat[i / w].push(elem)
-        }
-
-        mat
-    }
-
-    #[allow(unused)]
-    fn coord_to_idx(&self, coord: (usize, usize)) -> usize {
-        debug_assert!(coord.0 < self.shape.0 && coord.1 < self.shape.1);
-
-        coord.0 * self.shape.1 + coord.1
-    }
-
-    #[allow(unused)]
-    fn idx_to_coord(&self, idx: usize) -> (usize, usize) {
-        debug_assert!(idx < self.elem_num());
-
-        (idx / self.shape.1, idx % self.shape.1)
-    }
-
-    pub fn get(&self, coord: (usize, usize)) -> &T {
-        let idx = self.coord_to_idx(coord);
-
-        &self.flattened[idx]
-    }
-
-    pub fn set(&mut self, coord: (usize, usize), val: T) {
-        let idx = self.coord_to_idx(coord);
-
-        self.flattened[idx] = val;
-    }
-
-    pub fn is_square(&self) -> bool {
-        self.shape.0 == self.shape.1
-    }
-
-    pub fn apply_to(&self, vec: &Vector<T>) -> Vector<T>
-    where
-        T: Clone + Add<T, Output = T> + Mul<T, Output = T>,
-    {
-        let (h, w) = self.shape;
-
-        assert_eq!(w, vec.len());
-
-        let calc_elem = |i: usize| {
-            let mut elem = self.get((i, 0)).clone() * vec.get(0).clone();
-
-            for j in 1..w {
-                elem = elem + self.get((i, j)).clone() * vec.get(j).clone();
+        while exp != 0 {
+            if exp % 2 == 1 {
+                ret.mat_mul_assign_mod(&mul, modulus.clone());
             }
 
-            elem
-        };
+            mul = mul.mat_mul(&mul);
+            exp /= 2;
+        }
 
-        (0..h).map(|i| calc_elem(i)).collect::<Vec<T>>().into()
+        ret
     }
 }
 
@@ -402,6 +361,166 @@ where
     }
 }
 
+impl<T> Rem<T> for Matrix<T>
+where
+    T: Clone + Rem<T, Output = T>,
+{
+    type Output = Matrix<T>;
+
+    fn rem(self, rhs: T) -> Self::Output {
+        let flattened: Vec<T> = self
+            .flattened
+            .iter()
+            .map(|x| x.clone() % rhs.clone())
+            .collect();
+
+        Self::Output {
+            shape: self.shape,
+            flattened,
+        }
+    }
+}
+
+impl<T> RemAssign<T> for Matrix<T>
+where
+    T: Clone + RemAssign<T>,
+{
+    fn rem_assign(&mut self, rhs: T) {
+        self.flattened.iter_mut().for_each(|x| *x %= rhs.clone());
+    }
+}
+
+impl<T> Matrix<T>
+where
+    T: Clone,
+{
+    pub fn new(shape: (usize, usize)) -> Self
+    where
+        T: Default,
+    {
+        assert!(shape.0 >= 1 && shape.1 >= 1);
+
+        Self {
+            shape,
+            flattened: vec![T::default(); shape.0 * shape.1],
+        }
+    }
+
+    pub fn from_flattened(shape: (usize, usize), flattened: Vec<T>) -> Self {
+        assert!(shape.0 >= 1 && shape.1 >= 1);
+        assert_eq!(shape.0 * shape.1, flattened.len());
+
+        Self { shape, flattened }
+    }
+
+    pub fn filled(shape: (usize, usize), x: T) -> Self {
+        Self::from_flattened(shape, vec![x.clone(); shape.0 * shape.1])
+    }
+
+    pub fn zero(shape: (usize, usize)) -> Self
+    where
+        T: Zero,
+    {
+        Self::from_flattened(shape, vec![T::zero(); shape.0 * shape.1])
+    }
+
+    pub fn one(shape: (usize, usize)) -> Self
+    where
+        T: One,
+    {
+        Self::from_flattened(shape, vec![T::one(); shape.0 * shape.1])
+    }
+
+    pub fn from_vector(vec: Vec<T>) -> Self {
+        Self {
+            shape: (vec.len(), 1),
+            flattened: vec,
+        }
+    }
+
+    pub fn shape(&self) -> (usize, usize) {
+        self.shape
+    }
+
+    pub fn flattened(self) -> Vec<T> {
+        self.flattened
+    }
+
+    pub fn elem_num(&self) -> usize {
+        self.shape.0 * self.shape.1
+    }
+
+    pub fn to_vec(self) -> Vec<Vec<T>> {
+        let (h, w) = self.shape;
+
+        let mut mat = vec![vec![]; h];
+        mat.iter_mut().for_each(|x| x.reserve(w));
+
+        for (i, elem) in self.flattened.into_iter().enumerate() {
+            mat[i / w].push(elem)
+        }
+
+        mat
+    }
+
+    #[allow(unused)]
+    fn coord_to_idx(&self, coord: (usize, usize)) -> usize {
+        debug_assert!(coord.0 < self.shape.0 && coord.1 < self.shape.1);
+
+        coord.0 * self.shape.1 + coord.1
+    }
+
+    #[allow(unused)]
+    fn idx_to_coord(&self, idx: usize) -> (usize, usize) {
+        debug_assert!(idx < self.elem_num());
+
+        (idx / self.shape.1, idx % self.shape.1)
+    }
+
+    pub fn get(&self, coord: (usize, usize)) -> &T {
+        let idx = self.coord_to_idx(coord);
+
+        &self.flattened[idx]
+    }
+
+    pub fn get_mut(&mut self, coord: (usize, usize)) -> &mut T {
+        let idx = self.coord_to_idx(coord);
+
+        &mut self.flattened[idx]
+    }
+
+    pub fn set(&mut self, coord: (usize, usize), val: T) {
+        let idx = self.coord_to_idx(coord);
+
+        self.flattened[idx] = val;
+    }
+
+    pub fn is_square(&self) -> bool {
+        self.shape.0 == self.shape.1
+    }
+
+    pub fn apply_to(&self, vec: &Vector<T>) -> Vector<T>
+    where
+        T: Clone + Add<T, Output = T> + Mul<T, Output = T>,
+    {
+        let (h, w) = self.shape;
+
+        assert_eq!(w, vec.len());
+
+        let calc_elem = |i: usize| {
+            let mut elem = self.get((i, 0)).clone() * vec.get(0).clone();
+
+            for j in 1..w {
+                elem = elem + self.get((i, j)).clone() * vec.get(j).clone();
+            }
+
+            elem
+        };
+
+        (0..h).map(|i| calc_elem(i)).collect::<Vec<T>>().into()
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Vector<T>
 where
@@ -431,75 +550,6 @@ where
         }
 
         ret
-    }
-}
-
-impl<T> Vector<T>
-where
-    T: Clone,
-{
-    pub fn new(n: usize) -> Self
-    where
-        T: Default,
-    {
-        assert_ne!(n, 0);
-
-        vec![T::default(); n].into()
-    }
-
-    pub fn len(&self) -> usize {
-        self.elements.len()
-    }
-
-    pub fn elements(&self) -> &Vec<T> {
-        &self.elements
-    }
-
-    pub fn to_vec(self) -> Vec<T> {
-        self.elements
-    }
-
-    pub fn zero(n: usize) -> Self
-    where
-        T: Zero,
-    {
-        vec![T::zero(); n].into()
-    }
-
-    pub fn one(n: usize) -> Self
-    where
-        T: One,
-    {
-        vec![T::one(); n].into()
-    }
-
-    pub fn filled(x: T, n: usize) -> Self {
-        vec![x.clone(); n].into()
-    }
-
-    pub fn get(&self, idx: usize) -> &T {
-        &self.elements[idx]
-    }
-
-    pub fn apply_from(&self, mat: &Matrix<T>) -> Vector<T>
-    where
-        T: Clone + Add<T, Output = T> + Mul<T, Output = T>,
-    {
-        let (h, w) = mat.shape;
-
-        assert_eq!(self.len(), w);
-
-        let calc_elem = |i: usize| {
-            let mut elem = mat.get((i, 0)).clone() * self.get(0).clone();
-
-            for j in 1..w {
-                elem = elem + mat.get((i, j)).clone() * self.get(j).clone();
-            }
-
-            elem
-        };
-
-        (0..h).map(|i| calc_elem(i)).collect::<Vec<T>>().into()
     }
 }
 
@@ -604,6 +654,75 @@ where
     }
 }
 
+impl<T> Vector<T>
+where
+    T: Clone,
+{
+    pub fn new(n: usize) -> Self
+    where
+        T: Default,
+    {
+        assert_ne!(n, 0);
+
+        vec![T::default(); n].into()
+    }
+
+    pub fn len(&self) -> usize {
+        self.elements.len()
+    }
+
+    pub fn elements(&self) -> &Vec<T> {
+        &self.elements
+    }
+
+    pub fn to_vec(self) -> Vec<T> {
+        self.elements
+    }
+
+    pub fn zero(n: usize) -> Self
+    where
+        T: Zero,
+    {
+        vec![T::zero(); n].into()
+    }
+
+    pub fn one(n: usize) -> Self
+    where
+        T: One,
+    {
+        vec![T::one(); n].into()
+    }
+
+    pub fn filled(x: T, n: usize) -> Self {
+        vec![x.clone(); n].into()
+    }
+
+    pub fn get(&self, idx: usize) -> &T {
+        &self.elements[idx]
+    }
+
+    pub fn apply_from(&self, mat: &Matrix<T>) -> Vector<T>
+    where
+        T: Clone + Add<T, Output = T> + Mul<T, Output = T>,
+    {
+        let (h, w) = mat.shape;
+
+        assert_eq!(self.len(), w);
+
+        let calc_elem = |i: usize| {
+            let mut elem = mat.get((i, 0)).clone() * self.get(0).clone();
+
+            for j in 1..w {
+                elem = elem + mat.get((i, j)).clone() * self.get(j).clone();
+            }
+
+            elem
+        };
+
+        (0..h).map(|i| calc_elem(i)).collect::<Vec<T>>().into()
+    }
+}
+
 #[cfg(test)]
 mod test {
     mod test_for_matrix {
@@ -635,6 +754,27 @@ mod test {
             mat1.mat_mul_assign(&mat2);
 
             assert_eq!(mat1, vec![vec![31, 53], vec![18, 51]].into());
+        }
+
+        #[test]
+        fn test_mat_mul_mod() {
+            let mat1: Matrix<i32> = vec![vec![3, -1, 4], vec![-1, 5, 9]].into();
+            let mat2: Matrix<i32> = vec![vec![2, 6], vec![-5, -3], vec![5, 8]].into();
+
+            assert_eq!(
+                mat1.mat_mul_mod(&mat2, 10),
+                vec![vec![1, 3], vec![8, 1]].into()
+            );
+        }
+
+        #[test]
+        fn test_mat_mul_assign_mod() {
+            let mut mat1: Matrix<i32> = vec![vec![3, -1, 4], vec![-1, 5, 9]].into();
+            let mat2: Matrix<i32> = vec![vec![2, 6], vec![-5, -3], vec![5, 8]].into();
+
+            mat1.mat_mul_assign_mod(&mat2, 10);
+
+            assert_eq!(mat1, vec![vec![1, 3], vec![8, 1]].into());
         }
 
         #[test]
@@ -715,6 +855,38 @@ mod test {
             assert_eq!(
                 Matrix::<i32>::identity(5),
                 Matrix::identity(5).mat_pow(1_000_000_000_000_000_000)
+            );
+        }
+
+        #[test]
+        fn test_mat_pow_mod() {
+            let mat: Matrix<i32> = vec![vec![3, -1, 4], vec![-1, 5, 9], vec![2, 6, -5]].into();
+
+            assert_eq!(mat.mat_pow_mod(0, 10), Matrix::identity(3));
+
+            assert_eq!(
+                mat.mat_pow_mod(1, 5),
+                vec![vec![3, 4, 4], vec![4, 0, 4], vec![2, 1, 0]].into()
+            );
+
+            assert_eq!(
+                mat.mat_pow_mod(2, 10),
+                vec![vec![8, 6, 3], vec![0, 0, 6], vec![0, 8, 7]].into()
+            );
+
+            assert_eq!(
+                mat.mat_pow_mod(3, 10),
+                vec![vec![4, 0, 1], vec![2, 6, 0], vec![6, 2, 7]].into()
+            );
+
+            assert_eq!(
+                mat.mat_pow_mod(3, 1),
+                vec![vec![0, 0, 0], vec![0, 0, 0], vec![0, 0, 0]].into()
+            );
+
+            assert_eq!(
+                Matrix::<i32>::identity(5),
+                Matrix::identity(5).mat_pow(1_000_000_000_000_000_000),
             );
         }
 
