b62fe254683fbb85a7f8870c013888a2a014957c
atcoder8 <atcoder8@gmail.com>
2023-08-26 02:48:58 +0900

    Added functions `ext_gcd` and `naive_ext_gcd`.

--- src/number_theory.rs
+++ src/number_theory.rs
@@ -15,27 +15,51 @@ pub fn lcm(a: i64, b: i64) -> i64 {
     a / gcd(a, b) * b
 }
 
-pub fn ext_gcd(a: i64, b: i64) -> (i64, i64, i64) {
+/// Returns a tuple of `(x, y)` and `gcd(a, b)` that satisfy `ax + by = gcd(a, b)` in that order.
+///
+/// The returned `x`, `y` and `gcd(a, b)` satisfy the following:
+///   * Both `|x|` and `|y|` are less than or equal to `max(|a|, |b|)`.
+///   * `gcd(a, b)` is non-negative.
+///
+/// # Examples
+///
+/// ```
+/// # use atcoder8_library::number_theory::ext_gcd;
+///
+/// let (x, y, g) = ext_gcd(6, 10);
+///
+/// assert_eq!(g, 2);
+/// assert_eq!(6 * x + 10 * y, 2);
+/// assert!(x.abs() <= 10 && y.abs() <= 10);
+/// ```
+pub fn ext_gcd(mut a: i64, mut b: i64) -> (i64, i64, i64) {
+    if a == 0 && b == 0 {
+        return (0, 0, 0);
+    }
+
     let (mut s, mut t, mut u, mut v) = (1, 0, 0, 1);
-    let (mut a, mut b) = (a, b);
-
     while b != 0 {
-        let (q, r) = (a / b, a % b);
-
-        std::mem::swap(&mut s, &mut t);
-        t -= q * s;
-        std::mem::swap(&mut u, &mut v);
-        v -= q * u;
-
-        a = b;
-        b = r;
+        (a, b, s, t, u, v) = (b, a % b, t, s - a / b * t, v, u - a / b * v);
     }
 
-    if a >= 0 {
-        (s, u, a)
-    } else {
-        (-s, -u, -a)
+    let sgn = a.signum();
+    (sgn * s, sgn * u, sgn * a)
+}
+
+pub fn naive_ext_gcd(mut a: i64, mut b: i64) -> (i64, i64, i64) {
+    let mut quotients = vec![];
+    while b != 0 {
+        quotients.push(a / b);
+        (a, b) = (b, a % b);
     }
+
+    let (mut x, mut y) = (1, 0);
+    for &q in quotients.iter().rev() {
+        (x, y) = (y, x - q * y);
+    }
+
+    let sgn = a.signum();
+    (sgn * x, sgn * y, sgn * a)
 }
 
 pub fn crt(rr: &Vec<i64>, mm: &Vec<i64>) -> Option<(i64, i64)> {
@@ -65,4 +89,31 @@ pub fn crt(rr: &Vec<i64>, mm: &Vec<i64>) -> Option<(i64, i64)> {
     }
 
     Some((acc_r, acc_m))
+}
+
+#[cfg(test)]
+mod tests {
+    use num_integer::Integer;
+
+    use super::*;
+
+    fn check_validity_ext_gcd(a: i64, b: i64) {
+        let (x, y, g) = ext_gcd(a, b);
+
+        assert_eq!(g, a.gcd(&b));
+        assert_eq!(a * x + b * y, g);
+        assert!(x.abs().max(y.abs()) <= a.abs().max(b.abs()));
+
+        let boundary = a.abs().max(b.abs());
+        assert!(x.abs() <= boundary && y.abs() <= boundary);
+    }
+
+    #[test]
+    fn test_ext_gcd() {
+        for a in -100..=100 {
+            for b in -100..=100 {
+                check_validity_ext_gcd(a, b);
+            }
+        }
+    }
 }
