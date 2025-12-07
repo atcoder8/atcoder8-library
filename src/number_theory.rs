d8c9e277e9a706a6b0b10aeaea1c0f599351dc69
pencoder <atcoder8@gmail.com>
2025-12-07 19:22:22 +0900

    number_theoryモジュールのリファクタリングを行いました。

--- src/number_theory.rs
+++ src/number_theory.rs
@@ -20,24 +20,12 @@ pub fn lcm(a: i64, b: i64) -> i64 {
 /// The returned `x`, `y` and `gcd(a, b)` satisfy the following:
 ///   * Both `|x|` and `|y|` are less than or equal to `max(|a|, |b|)`.
 ///   * `gcd(a, b)` is non-negative.
-///
-/// # Examples
-///
-/// ```
-/// # use atcoder8_library::number_theory::ext_gcd;
-///
-/// let (x, y, g) = ext_gcd(6, 10);
-///
-/// assert_eq!(g, 2);
-/// assert_eq!(6 * x + 10 * y, 2);
-/// assert!(x.abs() <= 10 && y.abs() <= 10);
-/// ```
-pub fn ext_gcd(mut a: i64, mut b: i64) -> (i64, i64, i64) {
+pub fn ext_gcd(a: i64, b: i64) -> (i64, i64, i64) {
     if a == 0 && b == 0 {
         return (0, 0, 0);
     }
 
-    let (mut s, mut t, mut u, mut v) = (1, 0, 0, 1);
+    let (mut a, mut b, mut s, mut t, mut u, mut v) = (a, b, 1, 0, 0, 1);
     while b != 0 {
         (a, b, s, t, u, v) = (b, a % b, t, s - a / b * t, v, u - a / b * v);
     }
@@ -62,7 +50,7 @@ pub fn naive_ext_gcd(mut a: i64, mut b: i64) -> (i64, i64, i64) {
     (sgn * x, sgn * y, sgn * a)
 }
 
-pub fn crt(rr: &Vec<i64>, mm: &Vec<i64>) -> Option<(i64, i64)> {
+pub fn crt(rr: &[i64], mm: &[i64]) -> Option<(i64, i64)> {
     assert_eq!(rr.len(), mm.len());
     assert!(!rr.is_empty());
     assert!(mm.iter().all(|&m| m >= 1));
